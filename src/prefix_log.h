deae6dfde3af09123ef0a4f71f8af446033bbcf4
automan0608 <zhaosheng@ubuntu.(none)>
2016-06-27 16:56:51 +0800

    refine log

--- src/prefix_log.h
+++ src/prefix_log.h
@@ -4,18 +4,10 @@
 
 #include <stdio.h>
 
-#if 1
 
 #define prefix_log(level, fmt, arg...) do { 		\
 		printf("%5s | %s:%s:%d | " fmt "\n", level, __FILE__, __FUNCTION__, __LINE__, ##arg); 	\
 	} while (0)
 
-#else
-
-#define prefix_log(level, fmt) do {			\
-		printf(level); printf(" | "); printf(fmt); printf("\n");		\
-	} while (0)
-
-#endif
 
 #endif
